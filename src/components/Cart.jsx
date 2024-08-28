55ee99b007cec6f69d540459269a926cbd2dc8da
Venkatesh-Saravanan <ssvenkatesh1920@gmail.com>
2024-08-28 12:09:00 +0530

    initial commit

---
+++ src/components/Cart.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Cart = ({ cartItems, onRemoveFromCart, total }) => {
+
+    return (
+        <div>
+
+            {cartItems.length === 0 ? (
+                <p>Your cart is empty.</p>
+            ) : (
+                <ul className="list-group">
+                    {cartItems.map(item => (
+                        <li key={item.id} className="list-group-item d-flex justify-content-between align-items-center">
+                            {item.name} : ₹{(item.price).toLocaleString('en-IN')}
+                            <button className="btn btn-outline-danger btn-sm" onClick={() => onRemoveFromCart(item)}>Remove</button>
+                        </li>
+                    ))}
+                </ul>
+            )}
+            <p className="mt-3">Total Items: {cartItems.length}</p>
+            <p className="mt-3">Total Price: ₹{total.toLocaleString('en-IN')}</p>
+        </div>
+    );
+};
+
+export default Cart;
